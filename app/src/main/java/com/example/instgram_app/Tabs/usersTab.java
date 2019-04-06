94a680af62928a1cf513b3a00c4b92a68ea48862
HusseinReda97pro <47584580+HusseinReda97pro@users.noreply.github.com>
2019-04-06 08:06:07 +0200

    Read Users from back4App server and show it in users tab useing Listviee and Array Adapter

--- app/src/main/java/com/example/instgram_app/Tabs/usersTab.java
+++ app/src/main/java/com/example/instgram_app/Tabs/usersTab.java
@@ -6,15 +6,26 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.example.instgram_app.R;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class usersTab extends Fragment {
-
+    ListView usersList;
+    ArrayList arrayList;
+    ArrayAdapter arrayAdapter;
 
     public usersTab() {
         // Required empty public constructor
@@ -25,7 +36,27 @@ public class usersTab extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate( R.layout.fragment_users_tab, container, false );
+        View view = inflater.inflate( R.layout.fragment_users_tab, container, false );
+        usersList = view.findViewById( R.id.usersList );
+        arrayList = new ArrayList();
+        arrayAdapter = new ArrayAdapter( getContext(),android.R.layout.simple_list_item_1,arrayList);
+        ParseQuery<ParseUser> query = ParseUser.getQuery();
+        query.whereNotEqualTo( "username",ParseUser.getCurrentUser().getUsername() );
+        query.findInBackground( new FindCallback<ParseUser>() {
+            @Override
+            public void done(List<ParseUser> users, ParseException e) {
+                if (e==null){
+                    if(users.size()>0){
+                        for (ParseUser user:users){
+                            arrayList.add( user.getUsername() );
+                        }
+                        usersList.setAdapter( arrayAdapter );
+                    }
+                }
+            }
+        } );
+
+        return view;
     }
 
 }
